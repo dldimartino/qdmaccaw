69060d9150e483561e6f5311a5f5652329fe9070
Cesar Nina <c3sar207@gmail.com>
2020-05-06 23:58:36 -0400

    added a home component

---
+++ client/components/game/Home.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+
+export class Home extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h1>Skribbl 2.0</h1>
+        <button type="button">Play!</button>
+        <button type="button">Create Private Room</button>
+        <h2>About Us</h2>
+        <p>
+          skribbl 2.0 is a free multiplayer drawing and guessing game. One game
+          consists of a few rounds in which every round someone has to draw
+          their chosen word and others have to guess it to gain points! The
+          person with the most points at the end of game will then be crowned as
+          the winner!
+        </p>
+        <h2>How to Play</h2>
+        <p>
+          When its your turn to draw, you will have to choose a word from three
+          options and visualize that word in 80 seconds, alternatively when
+          somebody else is drawing you have to type your guess into the chat to
+          gain points, be quick, the earlier you guess a word the more points
+          you get!
+        </p>
+      </div>
+    )
+  }
+}
