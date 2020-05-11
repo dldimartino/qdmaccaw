ab2a084b37a56fb759febe637472f32d35aee012
Jared <usherjared@yahoo.com>
2020-05-11 11:03:40 -0400

    service workers

--- client/components/game/Home.js
+++ client/components/game/Home.js
@@ -7,10 +7,31 @@ export default class Home extends Component {
     this.state = {}
   }
   render() {
+    // This is the "Offline page" service worker
+    // Check compatibility for the browser we're running this in
+    if ('serviceWorker' in navigator) {
+      if (navigator.serviceWorker.controller) {
+        console.log(
+          '[PWA Builder] active service worker found, no need to register'
+        )
+      } else {
+        // Register the service worker
+        navigator.serviceWorker
+          .register('pwabuilder-sw.js', {
+            scope: '../../../'
+          })
+          .then(function(reg) {
+            console.log(
+              '[PWA Builder] Service worker has been registered for scope: ' +
+                reg.scope
+            )
+          })
+      }
+    }
+
     return (
       <div>
         <h1>Skribbl 2.0</h1>
-      
         <Link to="/play">
           <button type="button">Play!</button>
         </Link>
