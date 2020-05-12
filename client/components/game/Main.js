d1a9f1160ba1725e7322e9e2305a241c88785b4d
Jared <usherjared@yahoo.com>
2020-05-12 17:10:16 -0400

    Bootstrap 3

--- client/components/game/Main.js
+++ client/components/game/Main.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Button, Col, Row} from 'react-bootstrap'
+import {Button, Col, Row, Container} from 'react-bootstrap'
 
 const Main = () => {
   return (
-    <Col>
-      <h1>DrawBit</h1>
-      <Link to="/login">
-        <Button variant="success">Join Room</Button>
-      </Link>
-      <br />
-      <Link to="/signup">
-        <button type="button">Sign Up</button>
-      </Link>
-      <br />
-      <Link to="/">
-        <button type="button">Continue as Guest</button>
-      </Link>
-    </Col>
+    <Container>
+      <Row className="justify-content-md-center">
+        <h1>DrawBit</h1>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Link to="/login">
+          <Button variant="success">Join Room</Button>
+        </Link>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Link to="/signup">
+          <Button variant="primary">Sign Up</Button>
+        </Link>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Link to="/">
+          <Button variant="primary">Continue As Guest</Button>
+        </Link>
+      </Row>
+    </Container>
   )
 }
 
