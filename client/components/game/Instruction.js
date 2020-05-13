77dd292097dbd9b85f862820ef38b16929c9aa8e
Cesar Nina <c3sar207@gmail.com>
2020-05-13 00:21:36 -0400

    fix render bug in Create.js and added Rules in Instructions.js

--- client/components/game/Instruction.js
+++ client/components/game/Instruction.js
@@ -4,18 +4,35 @@ import {Link} from 'react-router-dom'
 export const Instruction = () => {
   return (
     <div>
-      <Link to="/">
-        <button type="button">Home</button>
+      <Link to="/main">
+        <button type="button">Back</button>
       </Link>
-      <p>
-        When its your turn to draw, you will have to choose a word from three
-        options and visualize that word in 80 seconds, alternatively when
-        somebody else is drawing you have to type your guess into the chat to
-        gain points, be quick, the earlier you guess a word the more points you
-        get!
-      </p>
-      <Link to="/">
-        <button type="button">Home</button>
+
+      <h3>Rules</h3>
+      <h4>Picturist</h4>
+      <ol>
+        <li>There's 1 Picturist a round</li>
+        <li>You will receive a word to draw</li>
+        <li>You have 30 second to draw it</li>
+      </ol>
+      <h4>Guesser</h4>
+      <ol>
+        <li>You have 30 second to guess</li>
+        <li>You only have 1 try to guess</li>
+      </ol>
+      <h4>Points</h4>
+      <ol>
+        <li>The earlier you guess, the more points you receive</li>
+        <li>Picturist will receive 1 point for each guess</li>
+        <li>Picturist will receieve 2x points if all guess</li>
+        <li>First to guess will multiply time left x3</li>
+        <li>Second to guess will multiply time left x2.5</li>
+        <li>Third to guess will multiply time left x2</li>
+        <li>Everyone else will receive points based on time</li>
+      </ol>
+
+      <Link to="/main">
+        <button type="button">Back</button>
       </Link>
     </div>
   )
