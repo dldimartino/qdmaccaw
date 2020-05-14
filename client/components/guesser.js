1579197cede3502fa4fe8651357e0e5e6942a3df
Daniel Stracquadanio <daniel.stracquadanio@gmail.com>
2020-05-13 23:01:33 -0400

    added whiteboard styling

--- client/components/guesser.js
+++ client/components/guesser.js
@@ -1,6 +1,7 @@
 import React, {createRef} from 'react'
 import io from 'socket.io-client'
 import CanvasDraw from 'react-canvas-draw'
+import {Col, Row, Container} from 'react-bootstrap'
 
 export default function Guesser() {
   const socket = io.connect(window.location.origin)
@@ -10,10 +11,16 @@ export default function Guesser() {
     canvas.current.loadSaveData(data, true)
   })
 
+  // prettier-ignore
   return (
-    <div>
-      <h1>Guess the drawing!</h1>
-      <CanvasDraw ref={canvas} disabled={true} hideInterface={true} />
-    </div>
+    <Container className="whiteboard">
+      <Row className="justify-content-md-center">
+        <h1 className="draw-word">Guess the drawing!</h1>
+      </Row>
+      <Row id="canvas" className="justify-content-md-center">
+       <CanvasDraw ref={canvas} disabled={true} hideInterface={true} hideGrid={true}
+          canvasHeight={window.innerHeight / 1.5} canvasWidth={window.innerWidth} />
+      </Row>
+    </Container>
   )
 }
--- client/components/reactWhiteboard.js
+++ client/components/reactWhiteboard.js
@@ -1,23 +1,91 @@
-import React, {createRef} from 'react'
+import React, {useState, createRef} from 'react'
 import CanvasDraw from 'react-canvas-draw'
 import io from 'socket.io-client'
+import {Col, Row, Container, Button, Collapse} from 'react-bootstrap'
+import {DropletFill} from 'react-bootstrap-icons'
 
 export default function ReactWhiteboard() {
+  const [color, setColor] = useState('#AAB7B8')
+  const [open, setOpen] = useState(false)
+  const [radius, setRadius] = useState(12)
   const socket = io.connect(window.location.origin)
-  console.log('socketSOCKET!!!! io.connect(window.location.origin)', socket)
   const canvas = createRef()
 
   function handleChange(event) {
-    console.log('Sending Drawing')
     socket.emit('drawing', event.getSaveData())
   }
 
   return (
-    <div>
-      <h1>Draw the word!</h1>
-      <div>
-        <CanvasDraw ref={canvas} onChange={handleChange} />
-      </div>
-    </div>
+    <Container className="whiteboard">
+      <Row className="justify-content-md-center">
+        <h1 className="draw-word">Draw the word!</h1>
+      </Row>
+      <Row id="canvas" className="justify-content-mid-center">
+        <CanvasDraw
+          ref={canvas}
+          onChange={handleChange}
+          hideInterface={true}
+          hideGrid={true}
+          brushColor={color}
+          brushRadius={radius}
+          canvasHeight={window.innerHeight / 1.5}
+          canvasWidth={window.innerWidth}
+        />
+        <div>
+          <Button
+            className="btn-dark"
+            onClick={() => setOpen(!open)}
+            aria-controls="collapse-buttons"
+            aria-expanded={open}
+          >
+            <DropletFill color={color} size={40} />
+          </Button>
+          <Collapse in={open}>
+            <div id="collapse-buttons">
+              <Row>
+                <Button
+                  className="palette"
+                  type="button"
+                  style={{backgroundColor: '#AAB7B8'}}
+                  onClick={(event) =>
+                    setColor(event.target.style.backgroundColor)
+                  }
+                />
+              </Row>
+              <Row>
+                <Button
+                  className="palette"
+                  type="button"
+                  style={{backgroundColor: 'red'}}
+                  onClick={(event) =>
+                    setColor(event.target.style.backgroundColor)
+                  }
+                />
+              </Row>
+              <Row>
+                <Button
+                  className="palette"
+                  type="button"
+                  style={{backgroundColor: 'blue'}}
+                  onClick={(event) =>
+                    setColor(event.target.style.backgroundColor)
+                  }
+                />
+              </Row>
+              <Row>
+                <Button
+                  className="palette"
+                  type="button"
+                  style={{backgroundColor: 'green'}}
+                  onClick={(event) =>
+                    setColor(event.target.style.backgroundColor)
+                  }
+                />
+              </Row>
+            </div>
+          </Collapse>
+        </div>
+      </Row>
+    </Container>
   )
 }
