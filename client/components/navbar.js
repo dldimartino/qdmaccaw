d673088a62e9ce8051029f998091bf0c8214e92c
Jared <usherjared@yahoo.com>
2020-05-15 15:25:09 -0400

    added music player

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,42 +4,21 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/">Home</Link>
-          <Link to="/whiteboard">WhiteBoard</Link>
-          <Link to="/guesser">Guess Word</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/">Home</Link>
-          <Link to="/whiteboard">WhiteBoard</Link>
-          <Link to="/guesser">Guess Word</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+const Navbar = () => {
+  return (
+    <div>
       <iframe
-        style={{border: 0, width: 500, height: 42}}
-        src="https://bandcamp.com/EmbeddedPlayer/album=1051830778/size=small/bgcol=ffffff/linkcol=0687f5/track=2184348779/transparent=true/"
+        style={{border: 0, width: 330, height: 42}}
+        src="https://bandcamp.com/EmbeddedPlayer/album=1051830778/size=small/bgcol=333333/linkcol=ffffff/track=3299718613/transparent=true/"
         seamless
       >
         <a href="http://robuwaldorf.bandcamp.com/album/video-game-music">
-          Video Game Music by Robu Waldorf
+          Music by Robu Waldorf
         </a>
       </iframe>
-    </nav>
-    <hr />
-  </div>
-)
+    </div>
+  )
+}
 
 /**
  * CONTAINER
