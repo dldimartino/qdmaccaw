4ec60f321516698c09f5b4ec95ea317ba8c86d88
Cesar Nina <c3sar207@gmail.com>
2020-05-15 15:54:40 -0400

    updated bootstrap

--- client/components/game/Create.js
+++ client/components/game/Create.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
+import {Button, Row, Container} from 'react-bootstrap'
 import {newRoom} from '../../store/allRoom'
 
 export class Create extends Component {
@@ -30,24 +31,38 @@ export class Create extends Component {
 
   render() {
     return (
-      <div>
-        <Link to="/main">
-          <button type="button">Back</button>
-        </Link>
-
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-
-          <br />
-          <button type="submit">Create</button>
-        </form>
-      </div>
+      <Container className="Buttons">
+        <Row className="justify-content-md-center ">
+          <h1 className="Welcome">DrawBit</h1>
+        </Row>
+        <div className="mb-4">
+          <Row className="justify-content-md-center ">
+            <Link to="/main">
+              <Button variant="danger" size="lg" className="shadow-lg">
+                Back
+              </Button>
+            </Link>
+          </Row>
+        </div>
+        <Row className="justify-content-md-center ">
+          <form onSubmit={this.handleSubmit}>
+            {/* <label htmlFor="name">Name:</label> */}
+            <input
+              type="text"
+              name="name"
+              placeholder="Room Name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <br />
+            <Row className="justify-content-md-center ">
+              <Button type="submit" className="mt-2" variant="primary">
+                Create
+              </Button>
+            </Row>
+          </form>
+        </Row>
+      </Container>
     )
   }
 }
