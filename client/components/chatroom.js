c075b438e1df302f1aac6e0401ca66ba086a6f82
Daniel Stracquadanio <daniel.stracquadanio@gmail.com>
2020-05-21 21:54:24 -0400

    Updated Chat

--- client/components/chatroom.js
+++ client/components/chatroom.js
@@ -74,66 +74,103 @@ export default class Chatroom extends Component {
 
   render() {
     return (
-      <div className="chat-container">
-        <header className="chat-header">
-          <h1>Welcome to {this.state.room.name}!</h1>
-          <br />
-          <h1>
-            <i>
-              <AiFillWechat /> Lobby
-            </i>
-          </h1>
-        </header>
-        <main className="chat-main">
-          <div className="chat-sidebar">
-            <h3>Current Artist:</h3>
-            <h3>{this.props.currentArtist.name}</h3>
-            {/* {this.props.inRoom.map((user) => {
-              if (user.isArtist) {
-                return (
-                  <h2 id="artist-name" key={user.id}>
-                    {user.name}
-                  </h2>
-                )
-              }
-            })} */}
-            <h3>Users in Lobby:</h3>
-            <ul>
-              {this.props.inRoom.map((user) => {
-                return <li key={user.id}>{user.name}</li>
-              })}
-            </ul>
-          </div>
-          <div className="chat-messages">
-            {this.state.messages.map((message, index) => (
-              <div className="message" key={index}>
-                <p className="meta">
-                  {message.name} <span>{message.timestamp}</span>
-                </p>
-                <p className="text">{message.message}</p>
-              </div>
-            ))}
-          </div>
-        </main>
-        <div className="chat-form-container">
-          <form id="chat-form" onSubmit={this.handleSubmit}>
-            <Link to="/FindRoom" className="link" onClick={this.handleClick}>
-              <Button type="button" variant="danger" className="chat-button">
-                Leave Lobby
+      <div>
+        <div className="chat-container">
+          <header className="chat-header">
+            <h1>Welcome to {this.state.room.name}!</h1>
+            <br />
+            <h1>
+              <i>
+                <AiFillWechat /> Lobby
+              </i>
+            </h1>
+          </header>
+          <main className="chat-main">
+            <div className="chat-sidebar">
+              <h2>
+                Current Artist:
+                {this.props.user.isArtist
+                  ? 'YOU!'
+                  : this.props.currentArtist.name}
+              </h2>
+              {this.props.user.name === this.props.currentArtist.name ? (
+                <div className="chat-sidebar">
+                  <br />
+                  <h1>Your word is: {this.props.gameWord} </h1>
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={this.props.handlePass}
+                    className="artist-button"
+                  >
+                    Pass The Paintbrush
+                  </Button>
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={this.props.wordGenerator}
+                    className="artist-button"
+                  >
+                    Regenerate Word
+                  </Button>
+                </div>
+              ) : (
+                <h4>...Waiting for Artist to start the game</h4>
+              )}
+              <h3>Users in Lobby:</h3>
+              <ul>
+                {this.props.inRoom.map((user) => {
+                  return <li key={user.id}>{user.name}</li>
+                })}
+              </ul>
+            </div>
+            <div className="chat-messages">
+              {this.state.messages.map((message, index) => (
+                <div className="message" key={index}>
+                  <p className="meta">
+                    {message.name} <span>{message.timestamp}</span>
+                  </p>
+                  <p className="text">{message.message}</p>
+                </div>
+              ))}
+            </div>
+          </main>
+          <div className="chat-form-container">
+            <form id="chat-form" onSubmit={this.handleSubmit}>
+              <Link
+                to="/FindRoom"
+                className="link"
+                onClick={this.props.handleClick}
+              >
+                <Button type="button" variant="danger" className="chat-button">
+                  Leave Lobby
+                </Button>
+              </Link>
+              <input
+                name="message"
+                type="text"
+                placeholder="Enter Message"
+                value={this.state.message}
+                onChange={this.handleChange}
+              />
+              <Button type="submit" variant="primary" className="chat-button">
+                Send <AiOutlineSend />
               </Button>
-            </Link>
-            <input
-              name="message"
-              type="text"
-              placeholder="Enter Message"
-              value={this.state.message}
-              onChange={this.handleChange}
-            />
-            <Button type="submit" variant="success" className="chat-button">
-              Send <AiOutlineSend />
-            </Button>
-          </form>
+            </form>
+          </div>
         </div>
+        {this.props.user.isArtist && (
+          <Button
+            type="button"
+            className="start-game"
+            variant="success"
+            onClick={this.props.startGame}
+            size="lg"
+            block
+          >
+            Start Game!
+          </Button>
+        )}
       </div>
     )
   }
